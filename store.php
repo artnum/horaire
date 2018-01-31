91065753a419d236065ad2969a44922e8356e7c7
artnum <etienne@artisan-numerique.ch>
2018-01-31 13:26:19 +0100

    Correction db path

--- store.php
+++ store.php
@@ -5,7 +5,7 @@ require('artnum/autoload.php');
 $http_request = new artnum\HTTP\JsonRequest();
 $store = new artnum\JStore\Generic($http_request, true);
 
-$pdo_db = new PDO("sqlite:/srv/apache-data/airtime.sqlite");
+$pdo_db = new PDO("sqlite:db/airtime.sqlite");
 $store->add_db('sql', $pdo_db);
 
 $store->run();
