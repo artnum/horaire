ac21616dd1a8355dbcfa143b987bd66a49bb0ab4
artnum <etienne@artisan-numerique.ch>
2018-08-08 12:10:39 +0200

    Dojo 13 and standardjs

--- widgets/People.js
+++ widgets/People.js
@@ -1,170 +1,169 @@
+/* eslint-env browser,amd */
 define([
-	"dojo/_base/declare",
-	"dijit/_WidgetBase",
-	"dijit/_TemplatedMixin",
-	"dijit/_WidgetsInTemplateMixin",
-	"dojo/_base/lang",
-	"dojo/text!./templates/People.html",
-	"dojo/request/xhr",
-	"dojo/on",
-	"dojo/parser",
-	"dojo/dom-construct",
-	"dojo/dom-class",
+  'dojo/_base/declare',
+  'dijit/_WidgetBase',
+  'dijit/_TemplatedMixin',
+  'dijit/_WidgetsInTemplateMixin',
+  'dojo/_base/lang',
+  'dojo/text!./templates/People.html',
+  'dojo/request/xhr',
+  'dojo/on',
+  'dojo/parser',
+  'dojo/dom-construct',
+  'dojo/dom-class',
 
-	"dijit/layout/StackContainer",
-	"dijit/layout/ContentPane",
-	"dijit/Dialog",
-	"dijit/registry",
+  'dijit/layout/StackContainer',
+  'dijit/layout/ContentPane',
+  'dijit/Dialog',
+  'dijit/registry',
 
-	"horaire/Button",
-	"horaire/Entity",
-	"horaire/_Result"
+  'horaire/Button',
+  'horaire/Entity',
+  'horaire/_Result'
 ], function (
-	djDeclare,
-	_dtWidgetBase,
-	_dtTemplatedMixin,
-	_dtWidgetsInTemplateMixin,
-	djLang,
-	template,
-	djXhr, 
-	djOn,
-	djParser,
-	djDomConstruct,
-	djDomClass,
+  djDeclare,
+  _dtWidgetBase,
+  _dtTemplatedMixin,
+  _dtWidgetsInTemplateMixin,
+  djLang,
+  template,
+  djXhr,
+  djOn,
+  djParser,
+  djDomConstruct,
+  djDomClass,
 
-	dtStackContainer,
-	dtContentPane,
-	dtDialog,
-	dtRegistry,
+  dtStackContainer,
+  DtContentPane,
+  DtDialog,
+  dtRegistry,
 
-	hButton,
-	hEntity,
-	_Result
-){
-	
-	return djDeclare('horaire.People', [
-		_dtWidgetBase, _dtTemplatedMixin, _dtWidgetsInTemplateMixin
-	],{
-		templateString: template,
-		baseClass: "people",
-		postCreate: function () {
-			var that = this;
+  hButton,
+  HEntity,
+  _Result
+) {
+  return djDeclare('horaire.People', [
+    _dtWidgetBase, _dtTemplatedMixin, _dtWidgetsInTemplateMixin
+  ], {
+    templateString: template,
+    baseClass: 'people',
+    postCreate: function () {
+      var that = this
 
-			var cp = new dtContentPane({  title: 'Accueil', id: "home" });
-			djXhr.get('/horaire/Entity', {handleAs: 'json'}).then( function ( results ) {
-				results = new _Result(results);
-				if(results.success()) {
-					var frag = document.createDocumentFragment();
-					results.whole().forEach( function ( entry ) {
-						var ecp = new dtContentPane({ id: 'P_' + entry.id, title: entry.commonName });
-						var entity = new hEntity(entry, { pane: ecp, link: 'P_' + entry.id} );
-						that.own(entity);
-						that.nContent.addChild(ecp);
-						
-						frag.appendChild(entity.domNode);
-					});
-					
-					window.requestAnimationFrame(function () {
-						that.nContent.addChild(cp); 
-						cp.set('content', frag); 
-						if(window.location.hash) {
-							that.nContent.selectChild(window.location.hash.substr(1));
-						} else {
-							that.nContent.selectChild('home'); 
-						}
-						that.nContent.startup(); 
-					});
-				}
-			});	
-		
-			djOn(this.nNewProject, "click", djLang.hitch(this, this.newProjectEvt));
-			djOn(this.nNewPerson, "click", djLang.hitch(this, this.newPersonEvt));
-			djOn(this.nHome, "click", djLang.hitch(this, function() { window.location.hash = '#home'; }));
+      var cp = new DtContentPane({ title: 'Accueil', id: 'home' })
+      djXhr.get('/horaire/Entity', {handleAs: 'json'}).then(function (results) {
+        results = new _Result(results)
+        if (results.success()) {
+          var frag = document.createDocumentFragment()
+          results.whole().forEach(function (entry) {
+            var ecp = new DtContentPane({ id: 'P_' + entry.id, title: entry.commonName })
+            var entity = new HEntity(entry, {pane: ecp, link: 'P_' + entry.id})
+            that.own(entity)
+            that.nContent.addChild(ecp)
 
-			djOn(window, "hashchange", djLang.hitch(this, function(e) { 
-				try {
-					this.nContent.selectChild(window.location.hash.substr(1));
-				} catch (e) {
-					this.error('Destination inconnue');
-				}
-			}));
-		},
+            frag.appendChild(entity.domNode)
+          })
 
-		error: function (msg) {
-			console.log(msg);
-		},
+          window.requestAnimationFrame(function () {
+            that.nContent.addChild(cp)
+            cp.set('content', frag)
+            if (window.location.hash) {
+              that.nContent.selectChild(window.location.hash.substr(1))
+            } else {
+              that.nContent.selectChild('home')
+            }
+            that.nContent.startup()
+          })
+        }
+      })
 
-		popForm: function (url, title, evts) {
-			var that = this;
-			var body = document.getElementsByTagName('BODY')[0];
-			
-			djDomClass.add(body, "waiting");
-			djXhr.get(url, { handleAs: "text"}).then (function (html) {
-				var html = djDomConstruct.toDom(html);
-				djParser.parse(html, { noStart: true}).then(function (dom) {
-					var dialog = new dtDialog({title: title});
-					dialog.addChild(dom[0]);
-					dialog.show();
-					
-					for(var k in evts) {
-						var hitch = that;
-						if(evts[k].hitch)	 {
-							hitch = evts[k].hitch;	
-						}
-						
-						djOn(dialog.domNode.getElementsByTagName('FORM')[0], k, djLang.hitch(hitch, evts.func));
-						djDomClass.remove(body, "waiting");
-					}
-				});			
-			});
-		},
+      djOn(this.nNewProject, 'click', djLang.hitch(this, this.newProjectEvt))
+      djOn(this.nNewPerson, 'click', djLang.hitch(this, this.newPersonEvt))
+      djOn(this.nHome, 'click', djLang.hitch(this, function () { window.location.hash = '#home' }))
 
-		newPersonEvt: function() {
-			
-		},
+      djOn(window, 'hashchange', djLang.hitch(this, function (e) {
+        try {
+          this.nContent.selectChild(window.location.hash.substr(1))
+        } catch (e) {
+          this.error('Destination inconnue')
+        }
+      }))
+    },
 
-		newProjectEvt: function () {
-			var that = this;
-			this.popForm('/horaire/html/newProject.html', 'Nouveau Projet', {
-					"submit" : { "func" : this.newProjectEx }
-				});
-		},
-		newProjectEx: function(event) {
-			event.preventDefault();
-			var form = dtRegistry.byId(event.target.getAttribute('widgetid'));
-			if(! form) { return ; }  /* not a widget ignore */
+    error: function (msg) {
+      console.log(msg)
+    },
 
-			if(form.isValid()) {
-				var values = form.getValues();
+    popForm: function (url, title, evts) {
+      var that = this
+      var body = document.getElementsByTagName('BODY')[0]
 
-				djXhr.get('/horaire/Project', { handleAs: "json", query: { "search.name": values.pName }}).then( function (results) {
-					results = new _Result(results);
-					var proceed = false;
-					if(results.empty()) {
-						proceed = true;	
-					} else {
-						console.log(results);
-						if(confirm('Un projet portant ce nom existe déjà. Créer quand même ?')) {
-							proceed = true;	
-						}
-					}
-					if(proceed) {
-						var eDate = null
-						if(values.pEndDate) {
-							eDate = values.pEndDate.toISOString();	
-						}
-						djXhr.post('/horaire/Project', { handleAs: "json", data: {
-									name: values.pName, 
-									targetEnd: eDate
-								}}).then(function ( results ) { 
-							results = new _Result(results);
-							console.log(results);
-						});
-					}
-				});
-			}
-		}
-		
+      djDomClass.add(body, 'waiting')
+      djXhr.get(url, {handleAs: 'text'}).then(function (html) {
+        html = djDomConstruct.toDom(html)
+        djParser.parse(html, {noStart: true}).then(function (dom) {
+          var dialog = new DtDialog({title: title})
+          dialog.addChild(dom[0])
+          dialog.show()
 
-	});
-});
+          for (var k in evts) {
+            var hitch = that
+            if (evts[k].hitch) {
+              hitch = evts[k].hitch
+            }
+
+            djOn(dialog.domNode.getElementsByTagName('FORM')[0], k, djLang.hitch(hitch, evts.func))
+            djDomClass.remove(body, 'waiting')
+          }
+        })
+      })
+    },
+
+    newPersonEvt: function () {
+
+    },
+
+    newProjectEvt: function () {
+      this.popForm('/horaire/html/newProject.html', 'Nouveau Projet', {
+        'submit': { 'func': this.newProjectEx }
+      })
+    },
+    newProjectEx: function (event) {
+      event.preventDefault()
+      var form = dtRegistry.byId(event.target.getAttribute('widgetid'))
+      if (!form) { return } /* not a widget ignore */
+
+      if (form.isValid()) {
+        var values = form.getValues()
+
+        djXhr.get('/horaire/Project', {handleAs: 'json', query: { 'search.name': values.pName }}).then(function (results) {
+          results = new _Result(results)
+          var proceed = false
+          if (results.empty()) {
+            proceed = true
+          } else {
+            console.log(results)
+            if (confirm('Un projet portant ce nom existe déjà. Créer quand même ?')) {
+              proceed = true
+            }
+          }
+          if (proceed) {
+            var eDate = null
+            if (values.pEndDate) {
+              eDate = values.pEndDate.toISOString()
+            }
+            djXhr.post('/horaire/Project', { handleAs: 'json',
+              data: {
+                name: values.pName,
+                targetEnd: eDate
+              }}).then(function (results) {
+              results = new _Result(results)
+              console.log(results)
+            })
+          }
+        })
+      }
+    }
+
+  })
+})
--- widgets/TimeSheet.js
+++ widgets/TimeSheet.js
@@ -1,196 +1,199 @@
+/* eslint-env amd,browser */
 define([
-	/* dojo base */
-	"dojo/_base/declare",
-	"dojo/_base/lang",
-	
-	/* dijit widget base */
-	"dijit/_WidgetBase",
-	"dijit/_TemplatedMixin",
-	"dijit/_WidgetsInTemplateMixin",
+  /* dojo base */
+  'dojo/_base/declare',
+  'dojo/_base/lang',
 
-	/* widget template */
-	"dojo/text!./templates/TimeSheet.html",
+  /* dijit widget base */
+  'dijit/_WidgetBase',
+  'dijit/_TemplatedMixin',
+  'dijit/_WidgetsInTemplateMixin',
 
-	/* utils */
-	"dojo/dom-construct",
-	"dojo/Evented",
-	"dojo/on",
-	"dojo/Deferred",
-	"dojo/when",
+  /* widget template */
+  'dojo/text!./templates/TimeSheet.html',
 
-	/* widgets */
-	"dijit/layout/TabContainer",
-	"dijit/layout/ContentPane",
-	"dijit/form/Select"
+  /* utils */
+  'dojo/dom-construct',
+  'dojo/Evented',
+  'dojo/on',
+  'dojo/Deferred',
+  'dojo/when',
 
-	/* generic script injection */
+  /* widgets */
+  'dijit/layout/TabContainer',
+  'dijit/layout/ContentPane',
+  'dijit/form/Select'
+
+  /* generic script injection */
 
 ], function (
-	/* dojo basa */
-	djDeclare,
-	djLang,
-	
-	/* dijit widget base */
-	dtWidgetBase,
-	dtTemplatedMixin,
-	dtWidgetsInTemplateMixin,
+  /* dojo basa */
+  djDeclare,
+  djLang,
 
-	/* widget template */
-	_template,
+  /* dijit widget base */
+  dtWidgetBase,
+  dtTemplatedMixin,
+  dtWidgetsInTemplateMixin,
 
-	/* utils */
-	djDomConstruct,
-	djEvented,
-	djOn,
-	djDeferred,
-	djWhen,
+  /* widget template */
+  _template,
 
-	/* widgets */
-	dtTabContainer,
-	dtContentPane,
-	dtFormSelect
+  /* utils */
+  djDomConstruct,
+  djEvented,
+  djOn,
+  DjDeferred,
+  djWhen,
 
-) { return djDeclare("airTime.TimeSheet", [ dtWidgetBase, dtTemplatedMixin, dtWidgetsInTemplateMixin, djEvented ], {
+  /* widgets */
+  dtTabContainer,
+  DtContentPane,
+  dtFormSelect
 
-	templateString: _template,
-	baseClass: "TimeSheet",
-	month: 0,
-	addFormNode: null,
-	year: 2017,
-	doValidate: null,
-	isValidated: null,
-	lockDef: null,
-	firstRun: true,
-	addWorktime: function(wt) {
-		this.own(wt);
-		this.workTime = wt;
-	},
-	lock: function() {
-		if(this.lockDef == null) {
-			this.lockDef = new djDeferred();
-			return new djDeferred().resolve();
-		} else {
-			var x = this.lockDef;
-			this.lockDef = new djDeferred();	
-			x.reject('in progress');
-			return x;
-		}
-	},
-	unlock: function() {
-		if(this.lockDef != null) {
-			this.lockDef.resolve();	
-			this.lockDef = null;
-		}
-	},
-	_setTitleAttr: { node: "n_Title", type: "innerHTML" },
-	_setWorkedAttr: function (value) {
-		this.n_WdTime.innerHTML = this.formatTime(value);
-	},
-	_setOverAttr: function (value) {
-		this.n_OrTime.innerHTML = this.formatTime(value);
-	}, 
-	_setTodoAttr: function (value) {
-		this.n_ToTime.innerHTML = this.formatTime(value);
-	},
-	_setSoldAttr: function (value) {
-		this.n_Sold.innerHTML = this.formatTime(value);
-	},
-	_setVacationsAttr: function(value) {
-		this.n_Vacations.innerHTML = value;
-	},
-	_setTotalVacationsAttr: function(value) {
-		this.n_TotalVacations.innerHTML = value;
-	},
-	_setToMonthAttr: function(value) {
-		this.month = value;
-		this.n_toMonth.set('value', value);
-		this.firstRun = false;
-	},
-	days: {},
-	workTime: null,
-	constructor: function(args) {
-		this.workTime = null;
-		this.days = {};
-		djLang.mixin(this, args);
-	},
-	formatTime: function(value) {
-		var hour = 0, minute = 0, neg = 0;
-		if(value < 0) { neg = 1; }
-		value = Math.abs(value) / 60;
-		
-		hour = Math.trunc(value);
-		minute = Math.round((value - hour) * 60);
+) {
+  return djDeclare('airTime.TimeSheet', [ dtWidgetBase, dtTemplatedMixin, dtWidgetsInTemplateMixin, djEvented ], {
 
-		if(minute < 10) { minute = '0' + minute; }
-		if(hour < 10) { hour = '0' + hour; }
-		
-		return neg ? '-' + hour + ':' + minute : hour + ':' + minute;
-	},
-	addForm: function (title, form) {
-		var cp = new dtContentPane({ title: title });
-		this.own(cp);
-		cp.placeAt(this.n_Forms);
-		if(djLang.isArray(form)) {
-			form.forEach(function (e) {
-				djDomConstruct.place(e, cp.domNode);
-			});
-		} else {
-			form.placeAt(cp);
-			this.addFormNode = form;
-		}
-		
-	},
-	setSelectedDate: function (value) {
-		this.addFormNode.set('selectedDate', value);
-	},
-	addDay: function (dayWidget) {
-			this.own(dayWidget);
-			this.days[dayWidget.cssDay()] = dayWidget;
-			djDomConstruct.place(dayWidget.domNode, this.n_Details);
-	},
-	addEntry: function (entryWidget) {
-		var day = this.days[entryWidget.cssDay()];
-		if(day) {
-			day.addTime(entryWidget);
-			this.own(entryWidget);
-		}
-	},
-	removeEntry: function (entryId) {
-		for(var idx in this.days) {
-			if(this.days[idx].removeTime(entryId)) {
-				break;	
-			}
-		}	
-	},
-	checkValidation: function () {
-		if(this.workTime) {
-			if(this.workTime.isValidated(this.year, this.month)) {
-				this.n_Validate.set('value', true);	
-			}	else {
-				this.n_Validate.set('value', false);	
-			}
-		}
-	},
-	postCreate: function() {
-		this.inherited(arguments);
-		this.n_toMonth.set('value', this.month);
+    templateString: _template,
+    baseClass: 'TimeSheet',
+    month: 0,
+    addFormNode: null,
+    year: 2017,
+    doValidate: null,
+    isValidated: null,
+    lockDef: null,
+    firstRun: true,
+    addWorktime: function (wt) {
+      this.own(wt)
+      this.workTime = wt
+    },
+    lock: function () {
+      if (this.lockDef == null) {
+        this.lockDef = new DjDeferred()
+        return new DjDeferred().resolve()
+      } else {
+        var x = this.lockDef
+        this.lockDef = new DjDeferred()
+        x.reject('in progress')
+        return x
+      }
+    },
+    unlock: function () {
+      if (this.lockDef != null) {
+        this.lockDef.resolve()
+        this.lockDef = null
+      }
+    },
+    _setTitleAttr: { node: 'n_Title', type: 'innerHTML' },
+    _setWorkedAttr: function (value) {
+      this.n_WdTime.innerHTML = this.formatTime(value)
+    },
+    _setOverAttr: function (value) {
+      this.n_OrTime.innerHTML = this.formatTime(value)
+    },
+    _setTodoAttr: function (value) {
+      this.n_ToTime.innerHTML = this.formatTime(value)
+    },
+    _setSoldAttr: function (value) {
+      this.n_Sold.innerHTML = this.formatTime(value)
+    },
+    _setVacationsAttr: function (value) {
+      this.n_Vacations.innerHTML = value
+    },
+    _setTotalVacationsAttr: function (value) {
+      this.n_TotalVacations.innerHTML = value
+    },
+    _setToMonthAttr: function (value) {
+      this.month = value
+      this.n_toMonth.set('value', value)
+      this.firstRun = false
+    },
+    days: {},
+    workTime: null,
+    constructor: function (args) {
+      this.workTime = null
+      this.days = {}
+      djLang.mixin(this, args)
+    },
+    formatTime: function (value) {
+      var hour = 0
+      var minute = 0
+      var neg = 0
+      if (value < 0) { neg = 1 }
+      value = Math.abs(value) / 60
 
-		djOn(this.n_toMonth, "change", djLang.hitch(this, function (e) {
-			if(this.month != e) {
-				this.month = e;
-				this.emit("change-month", this);
-			}	
-		}));
+      hour = Math.trunc(value)
+      minute = Math.round((value - hour) * 60)
 
-		djOn(this.n_Validate, "click", djLang.hitch(this, function (e) {
-			if(this.doValidate) {
-				this.doValidate(this.n_Validate.get('value'), parseInt(this.month), parseInt(this.year));	
-			}
-		}));
+      if (minute < 10) { minute = '0' + minute }
+      if (hour < 10) { hour = '0' + hour }
 
-	},
-	_onChange: function (e) {
-		
-	}
+      return neg ? '-' + hour + ':' + minute : hour + ':' + minute
+    },
+    addForm: function (title, form) {
+      var cp = new DtContentPane({ title: title })
+      this.own(cp)
+      cp.placeAt(this.n_Forms)
+      if (djLang.isArray(form)) {
+        form.forEach(function (e) {
+          djDomConstruct.place(e, cp.domNode)
+        })
+      } else {
+        form.placeAt(cp)
+        this.addFormNode = form
+      }
+    },
+    setSelectedDate: function (value) {
+      this.addFormNode.set('selectedDate', value)
+    },
+    addDay: function (dayWidget) {
+      this.own(dayWidget)
+      this.days[dayWidget.cssDay()] = dayWidget
+      djDomConstruct.place(dayWidget.domNode, this.n_Details)
+    },
+    addEntry: function (entryWidget) {
+      var day = this.days[entryWidget.cssDay()]
+      if (day) {
+        day.addTime(entryWidget)
+        this.own(entryWidget)
+      }
+    },
+    removeEntry: function (entryId) {
+      for (var idx in this.days) {
+        if (this.days[idx].removeTime(entryId)) {
+          break
+        }
+      }
+    },
+    checkValidation: function () {
+      if (this.workTime) {
+        if (this.workTime.isValidated(this.year, this.month)) {
+          this.n_Validate.set('value', true)
+        } else {
+          this.n_Validate.set('value', false)
+        }
+      }
+    },
+    postCreate: function () {
+      this.inherited(arguments)
+      this.n_toMonth.set('value', this.month)
 
-});}); 
+      djOn(this.n_toMonth, 'change', djLang.hitch(this, function (e) {
+        if (this.month !== e) {
+          this.month = e
+          this.emit('change-month', this)
+        }
+      }))
+
+      djOn(this.n_Validate, 'click', djLang.hitch(this, function (e) {
+        if (this.doValidate) {
+          this.doValidate(this.n_Validate.get('value'), parseInt(this.month), parseInt(this.year))
+        }
+      }))
+    },
+    _onChange: function (e) {
+
+    }
+
+  })
+})
