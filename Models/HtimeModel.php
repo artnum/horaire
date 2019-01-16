85f487d434d87ae63e7503632cedd3a099cda400
artnum <etienne@artisan-numerique.ch>
2019-01-16 16:39:59 +0100

    Remove automatic conversion to date on day

--- Models/HtimeModel.php
+++ Models/HtimeModel.php
@@ -3,14 +3,13 @@
       function __construct($db, $config) {
          parent::__construct($db, 'htime', 'htime_id', $config);
          $this->conf('auto-increment', true);
-         $this->conf('datetime', array('day'));
          $this->conf('mtime', 'htime_modified');
          $this->conf('mtime.ts', true);
          $this->conf('create', 'htime_created');
          $this->conf('create.ts', true);
          $this->conf('delete', 'htime_deleted');
          $this->conf('delete.ts', true);
-         $this->set_req('get', 'SELECT * FROM "\\Table" LEFT JOIN "project" ON "\\Table"."htime_project" = "project"."project_id" LEFT JOIN "person" ON "\\Table"."htime_person" = "person"."person_id" WHERE "\\IDName" = :id');
+         $this->set_req('get', 'SELECT * FROM "\\Table" LEFT JOIN "project" ON "\\Table"."htime_project" = "project"."project_id" LEFT JOIN "person" ON "\\Table"."htime_person" = "person"."person_id" LEFT JOIN "process" ON "\\Table"."htime_process" = "process"."process_id" WHERE "\\IDName" = :id');
       }
    }
 ?>
