36f2f3f2418f1b24d447a9ea8f87dc0e1e4df9e5
artnum <etienne@artisan-numerique.ch>
2019-04-08 09:43:13 +0200

    Time are not kept on delete

--- Models/HtimeModel.php
+++ Models/HtimeModel.php
@@ -7,8 +7,6 @@
          $this->conf('mtime.ts', true);
          $this->conf('create', 'htime_created');
          $this->conf('create.ts', true);
-         $this->conf('delete', 'htime_deleted');
-         $this->conf('delete.ts', true);
          $this->set_req('get', 'SELECT * FROM "\\Table" LEFT JOIN "project" ON "\\Table"."htime_project" = "project"."project_id" LEFT JOIN "person" ON "\\Table"."htime_person" = "person"."person_id" LEFT JOIN "process" ON "\\Table"."htime_process" = "process"."process_id" WHERE "\\IDName" = :id');
       }
    }
