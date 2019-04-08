82e4c2cadbad34fd447f3c1360b762fa5c4bd84b
artnum <etienne@artisan-numerique.ch>
2019-04-08 17:23:39 +0200

    Add print for project

---
+++ admin/exec/export/bon.php
@@ -0,0 +1,157 @@
+<?PHP
+include('pdf.php');
+
+$pdo = new PDO('sqlite:../../../db/horaire.sqlite3');
+$pdo->exec('PRAGMA foreign_keys = YES;');
+
+if (isset($_GET['pid']) && is_numeric($_GET['pid'])) {
+  $st = $pdo->prepare('SELECT * FROM "project" LEFT JOIN "travail" ON "project"."project_id" = "travail"."travail_project" WHERE "project_id" = :id');
+  if (!$st) {
+    die('Erreur');
+  }
+  $st->bindParam(':id', $_GET['pid'], PDO::PARAM_INT);
+  if (!$st->execute()) {
+    die('Erreur');
+  }
+  if(!($data = $st->fetch())) {
+    die('Erreur');
+  }
+
+
+  $PDF = new HorairePDF();
+  $PDF->SetAutoPageBreak(false);
+  $PDF->SetMargins(15,30, 10);
+  $PDF->AddPage('P', 'a4');
+  $PDF->SetFont('helvetica', '', 12);
+  $PDF->addTab('middle');
+  $PDF->addTab(130);
+  $PDF->block('head');
+  $y = $PDF->GetY();
+  $PDF->SetY($y + 8);
+  $PDF->squaredFrame(37, array('line-type' => 'dotted', 'square' => 9, 'lined' => true));
+  $PDF->SetY($y);
+  foreach(array('project_reference' => 'N° de bon',
+              'travail_reference' => 'Référence',
+              'travail_meeting' => 'Rendez-vous',
+              'travail_contact' => 'Personne de contact',
+              'travail_phone' => 'Téléphone') as $item => $label) {
+    $PDF->tab(1);
+    $PDF->SetFont('helvetica', '', 8);
+    $PDF->printLn($label);
+    $PDF->tab(1);
+    $PDF->SetFont('helvetica', 'B', 10);
+    $PDF->printLn($data[$item] ? $data[$item] : ' ');
+  }
+  $PDF->reset();
+  foreach(array('1' => 'Client',
+              '2' => 'Téléphone',
+              '3' => 'Adresse') as $item => $label) {
+
+    $PDF->SetFont('helvetica', '', 8);
+    $PDF->printLn($label);
+    $PDF->SetFont('helvetica', 'B', 10);
+    $PDF->printLn('');
+  }
+
+  
+  $PDF->block('description');
+  $PDF->SetFont('helvetica', 'B', 10);
+  $PDF->printLn('Description du travail');
+  $PDF->hr();
+  $PDF->SetFont('helvetica', '', 10);
+  $PDF->printLn($data['travail_description'], array('multiline' => true));
+
+  $PDF->br();
+  $PDF->block('worktime');
+  $bHeight = 25;
+  $PDF->SetFont('helvetica', 'B', 10);
+  $PDF->printLn('Main d\'œuvre');
+  $PDF->br();
+  $y = $PDF->GetY();
+  $PDF->SetFont('helvetica', '', 10);
+  $furtherX = 0;
+  $PDF->Line($PDF->lMargin, ceil($y - $PDF->FontSize), ceil($PDF->w - $PDF->rMargin), ceil($y - $PDF->FontSize));
+  foreach(array('Date' => 30, 'Employé' => 90, 'Tarif' => 15, 'Heure' => 15, 'Total' => 0) as $label => $w) {
+    $PDF->Cell($w, 0, $label, 0, 0, 'C');
+    if ($w > 0) {
+      if ($PDF->GetX() > $furtherX) { $furtherX = $PDF->GetX(); }
+      $PDF->Line($PDF->GetX(), $y - $PDF->FontSize, $PDF->GetX(), ceil($y + $bHeight + 10));
+    }
+  }
+  $PDF->Line($PDF->lMargin, $y + $PDF->FontSize, ceil($PDF->w - $PDF->rMargin), $y + $PDF->FontSize);
+  $PDF->SetXY($PDF->lMargin, ceil($y + $PDF->FontSize + 6));
+  $PDF->squaredFrame($bHeight, array('square' => 5, 'lined' => true, 'line-type'=>'dotted'));
+  $PDF->SetY($PDF->GetY() + $bHeight + 2);
+  $str = 'Total main d\'œuvre : ';
+  $PDF->SetX(floor($furtherX - $PDF->GetStringWidth($str)));
+  $PDF->printLn($str);
+  $PDF->Line($PDF->lMargin, $PDF->GetY(), ceil($PDF->w - $PDF->rMargin), $PDF->GetY());
+  
+  $PDF->block('others');
+  $PDF->br();
+  $PDF->SetFont('helvetica', 'B', 10);
+  $PDF->printLn('Matériel utilisé et autres charge');
+  $PDF->br();
+  $bHeight = 35;
+  $y = $PDF->GetY();
+  $PDF->SetFont('helvetica', '', 10);
+  $furtherX = 0;
+  $PDF->Line($PDF->lMargin, $y - $PDF->FontSize, ceil($PDF->w - $PDF->rMargin), $y - $PDF->FontSize);
+  foreach(array('Matériaux' => 100, 'Qté' => 15, "Unité\n(km/h/pc)" => 15, 'Prix unité' => 15, 'Total' => 0) as $label => $w) {
+    $PDF->SetFont('helvetica', '', 10);
+    $sublabel = '';
+    if (strpos($label, "\n") !== FALSE) {
+      $label = explode("\n", $label);
+      $sublabel = $label[1];
+      $label = $label[0];
+      $PDF->SetFont('helvetica', '', 8);
+    }
+    $pX = $PDF->GetX();
+    $PDF->Cell($w, 0, $label, 0, 0, 'C');
+    if ($sublabel !== '') {
+      /*$oX = $PDF->GetX();
+      $PDF->SetFont('helvetica', '', 8);
+      $PDF->SetX($pX);
+      $oY = $PDF->GetY();
+      $PDF->SetY($oY + 4);
+      $PDF->Cell($w, 0, $sublabel, 0, 0, 'C');
+      $PDF->SetXY($oX, $oY);*/
+    }
+    if ($w > 0) {
+      if ($PDF->GetX() > $furtherX) { $furtherX = $PDF->GetX(); }
+      $PDF->Line($PDF->GetX(), $y - $PDF->FontSize, $PDF->GetX(), ceil($y + $bHeight + 10));
+    }
+  }
+  $PDF->Line($PDF->lMargin, $y + $PDF->FontSize, ceil($PDF->w - $PDF->rMargin), $y + $PDF->FontSize);
+  $PDF->SetXY($PDF->lMargin, ceil($y + $PDF->FontSize + 6));
+  $PDF->squaredFrame($bHeight, array('square' => 5, 'lined' => true, 'line-type'=>'dotted'));
+  $PDF->SetY($PDF->GetY() + $bHeight + 2);
+  $str = 'Total matériel et autres charges : ';
+  $PDF->SetX(floor($furtherX - $PDF->GetStringWidth($str)));
+  $PDF->printLn($str);
+  $PDF->Line($PDF->lMargin, $PDF->GetY(), ceil($PDF->w - $PDF->rMargin), $PDF->GetY());
+  
+
+  $PDF->block('remarks');
+  $PDF->br();
+  $PDF->SetFont('helvetica', 'B', 10);
+  $PDF->printLn('Observations/remarques');
+  $bHeight = 15;
+  $PDF->br();
+  $PDF->squaredFrame($bHeight, array('up-to' => 262, 'square' => 5, 'lined' => true, 'line-type'=>'dotted'));  
+
+  $PDF->block('sign');
+  $PDF->SetFont('helvetica', '', 10);
+  $PDF->br();
+  $PDF->printLn('Date :', array('break' => false));
+  $PDF->drawLine(ceil($PDF->GetX()  + 3), ceil($PDF->GetY() + 3), floor($PDF->getTab(1) - $PDF->GetX() -5), 0, 'dotted');
+  $PDF->tab(1);
+  $PDF->printLn('Signature du donneur d\'ordre :', array('break' => false));
+  $PDF->drawLine(ceil($PDF->GetX()  + 3), ceil($PDF->GetY() + 3), floor($PDF->w - $PDF->rMargin - $PDF->GetX() - 3), 0, 'dotted');
+  $PDF->br(); $PDF->br();
+  $PDF->printLn('Nom et prénom du signataire si différent du haut :', array('break' => false));
+  $PDF->drawLine(ceil($PDF->GetX()  + 3), ceil($PDF->GetY() + 3), floor($PDF->w - $PDF->rMargin - $PDF->GetX() - 3), 0, 'dotted');
+  $PDF->Output();
+}
+
+?>
---
+++ admin/exec/export/pdf.php
@@ -0,0 +1,23 @@
+<?PHP
+include('artnum/autoload.php');
+
+class HorairePDF extends artnum\PDF {
+  function __construct($options = array()) {
+    parent::__construct();
+    $this->SetMargins(15, 40, 10);
+    $this->SetFont('helvetica');
+  }
+
+  function Header() {
+    $this->SetXY(15, 15);
+    $this->SetFont('helvetica', 'B', 16);
+    $this->Cell(0, 0, 'Bon de Travail');
+  }
+  function Footer() {
+    $this->SetXY(15, $this->h - 10);
+    $this->Line($this->GetX(), floor($this->GetY() - 2), ceil($this->w - $this->rMargin), floor($this->GetY() - 2));
+    $this->SetFont('helvetica', '', 8);
+    $this->Cell($this->w - 30, 0, 'Righini Construction Métalliques — Ancienne Pointe 38 — CH-1920 Martigny', 'C');
+  }
+}
+?>
