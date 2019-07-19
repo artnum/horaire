993fd2294940961eef5bfac02eaa8ddb97eccb14
artnum <etienne@artisan-numerique.ch>
2019-07-19 16:01:22 +0200

    Indendation

--- Models/PersonModel.php
+++ Models/PersonModel.php
@@ -1,14 +1,14 @@
 <?PHP
-   class PersonModel extends artnum\SQL {
-      function __construct($db, $config) {
-         parent::__construct($db, 'person', 'person_id', $config);
-         $this->conf('auto-increment', true);
-         $this->conf('create', 'person_created');
-         $this->conf('create.ts', true);
-         $this->conf('mtime', 'person_modified');
-         $this->conf('mtime.ts', true);
-         $this->conf('delete', 'person_deleted');
-         $this->conf('delete.ts', true);
-      }
-   }
+class PersonModel extends artnum\SQL {
+  function __construct($db, $config) {
+    parent::__construct($db, 'person', 'person_id', $config);
+    $this->conf('auto-increment', true);
+    $this->conf('create', 'person_created');
+    $this->conf('create.ts', true);
+    $this->conf('mtime', 'person_modified');
+    $this->conf('mtime.ts', true);
+    $this->conf('delete', 'person_deleted');
+    $this->conf('delete.ts', true);
+  }
+}
 ?>
