d9f3f053c52022a0a62dda8e8109711036f467ad
artnum <etienne@artisan-numerique.ch>
2019-09-25 16:18:44 +0200

    Implémentation de la liste de matériel

--- admin/js/stores.js
+++ admin/js/stores.js
@@ -10,61 +10,114 @@ function highlight (val, txt, open = '<span class="match">', close = '</span>') 
   return txt
 }
 
-const STProject = {
-  get: (id) => {
-    return new Promise((resolve, reject) => {
-      let entry = null
-      if (id === undefined || id === null || id === false) { resolve(entry); return }
-      Artnum.Query.exec(Artnum.Path.url(`Project/${id}`)).then((results) => {
-        if (results.success && results.length === 1) {
-          let entry = Array.isArray(results.data) ? results.data[0] : results.data
-          entry.label = `${entry.reference} - ${entry.name}`
-          entry.value = entry.uid ? entry.uid : entry.id
-        }
-        resolve(entry)
-      })
-    })
-  },
-  query: (txt) => {
-    return new Promise((resolve, reject) => {
-      let entries = []
-      let searchTerm = txt.toAscii().toLowerCase()
-      let params = {
-        'search.reference': `~${searchTerm}%`,
-        'search.name': `~%${searchTerm}%`,
-        'search._rules': '(reference OR name) AND deleted AND closed',
-        'search.deleted': '-',
-        'search.closed': '-'
+const STProject = function (store) {
+  this.Store = store
+}
+
+STProject.prototype.get = function (id) {
+  return new Promise((resolve, reject) => {
+    let entry = null
+    if (id === undefined || id === null || id === false) { resolve(entry); return }
+    Artnum.Query.exec(Artnum.Path.url(`${this.Store}/${id}`)).then((results) => {
+      if (results.success && results.length === 1) {
+        entry = Array.isArray(results.data) ? results.data[0] : results.data
+        entry.label = `${entry.reference} - ${entry.name}`
+        entry.value = entry.uid ? entry.uid : entry.id
       }
-      Artnum.Query.exec(Artnum.Path.url(`Project`, {params: params})).then((results) => {
-        if (results.success && results.length) {
-          results.data.forEach((entry) => {
-            let name = `${entry.reference} ${entry.name}`
-            name = highlight(searchTerm, name)
-            entry.label = name
-            entry.value = entry.uid ? entry.uid : entry.id
-            entries.push(entry)
-          })
-        }
-        entries.sort((a, b) => {
-          let Xa = parseInt(a.reference)
-          let Xb = parseInt(b.reference)
-          if (isNaN(Xa)) { Xa = Infinity }
-          if (isNaN(Xb)) { Xb = Infinity }
-          if (Xa.toString() !== a.reference) { Xa = Infinity }
-          if (Xb.toString() !== b.reference) { Xb = Infinity }
-          if (Xa === Infinity && Xb === Infinity) {
-            return a.reference.localeCompare(b.reference)
-          }
-          return Xa - Xb
-        })
-
-        resolve(entries)
-      })
+      resolve(entry)
     })
-  },
-  getIdentity: (object) => {
-    return object.uid ? object.uid : object.id
-  }
+  })
+}
 
+STProject.prototype.query = function (txt) {
+  return new Promise((resolve, reject) => {
+    let entries = []
+    let searchTerm = txt.toAscii().toLowerCase()
+    let params = {
+      'search.reference': `~${searchTerm}%`,
+      'search.name': `~%${searchTerm}%`,
+      'search._rules': '(reference OR name) AND deleted AND closed',
+      'search.deleted': '-',
+      'search.closed': '-'
+    }
+    Artnum.Query.exec(Artnum.Path.url(`${this.Store}`, {params: params})).then((results) => {
+      if (results.success && results.length) {
+        results.data.forEach((entry) => {
+          let name = `${entry.reference} ${entry.name}`
+          name = highlight(searchTerm, name)
+          entry.label = name
+          entry.value = entry.uid ? entry.uid : entry.id
+          entries.push(entry)
+        })
+      }
+      entries.sort((a, b) => {
+        let Xa = parseInt(a.reference)
+        let Xb = parseInt(b.reference)
+        if (isNaN(Xa)) { Xa = Infinity }
+        if (isNaN(Xb)) { Xb = Infinity }
+        if (Xa.toString() !== a.reference) { Xa = Infinity }
+        if (Xb.toString() !== b.reference) { Xb = Infinity }
+        if (Xa === Infinity && Xb === Infinity) {
+          return a.reference.localeCompare(b.reference)
+        }
+        return Xa - Xb
+      })
+
+      resolve(entries)
+    })
+  })
+}
+
+STProject.prototype.getIdentity = function (object) {
+  return object.uid ? object.uid : object.id
+}
+
+const STCategory = function (store) {
+  this.Store = store
+}
+
+STCategory.prototype.get = function (id) {
+  return new Promise((resolve, reject) => {
+    let entry = null
+    if (id === undefined || id === null || id === false) { resolve(entry); return }
+    Artnum.Query.exec(Artnum.Path.url(`${this.Store}/${id}`)).then((results) => {
+      if (results.success && results.length === 1) {
+        entry = Array.isArray(results.data) ? results.data[0] : results.data
+        entry.label = `${entry.name}`
+        entry.value = entry.uid ? entry.uid : entry.id
+      }
+      resolve(entry)
+    })
+  })
+}
+
+STCategory.prototype.query = function (txt) {
+  return new Promise((resolve, reject) => {
+    let entries = []
+    let searchTerm = txt.toAscii().toLowerCase()
+    let params = {
+      'search.name': `~%${searchTerm}%`,
+      'search.deleted': '-'
+    }
+    Artnum.Query.exec(Artnum.Path.url(`${this.Store}`, {params: params})).then((results) => {
+      if (results.success && results.length) {
+        results.data.forEach((entry) => {
+          let name = `${entry.reference} ${entry.name}`
+          name = highlight(searchTerm, name)
+          entry.label = name
+          entry.value = entry.uid ? entry.uid : entry.id
+          entries.push(entry)
+        })
+      }
+      entries.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
+
+      resolve(entries)
+    })
+  })
+}
+
+STCategory.prototype.getIdentity = function (object) {
+  return object.uid ? object.uid : object.id
 }
