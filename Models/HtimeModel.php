6149f216ccb3a022297a379b658806da7ed1c59d
artnum <etienne@artisan-numerique.ch>
2019-11-27 10:47:57 +0100

    Adaptation aux modifications https://github.com/artnum/phplibs/commit/23900b5b8146e59edf4467b6c23fa63461cc8d22

--- Models/HtimeModel.php
+++ Models/HtimeModel.php
@@ -7,7 +7,7 @@
          $this->conf('mtime.ts', true);
          $this->conf('create', 'htime_created');
          $this->conf('create.ts', true);
-         $this->set_req('get', 'SELECT * FROM "\\Table" LEFT JOIN "project" ON "\\Table"."htime_project" = "project"."project_id" LEFT JOIN "person" ON "\\Table"."htime_person" = "person"."person_id" LEFT JOIN "process" ON "\\Table"."htime_process" = "process"."process_id" LEFT JOIN "travail" ON "\\Table"."htime_travail" = "travail"."travail_id" WHERE "\\IDName" = :id');
+         $this->set_req('get', 'SELECT * FROM "\\Table" LEFT JOIN "project" ON "\\Table"."htime_project" = "project"."project_id" LEFT JOIN "person" ON "\\Table"."htime_person" = "person"."person_id" LEFT JOIN "process" ON "\\Table"."htime_process" = "process"."process_id" LEFT JOIN "travail" ON "\\Table"."htime_travail" = "travail"."travail_id"');
       }
    }
 ?>
