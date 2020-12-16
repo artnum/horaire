5bb1d2ceb8e5581ca4ae989b7d6b3e37e04c8aae
artnum <etienne@artisan-numerique.ch>
2020-12-16 16:34:27 +0100

    Import/export vers xlsx

---
+++ admin/exec/export/bill.php
@@ -0,0 +1,145 @@
+<?PHP
+require('artnum/autoload.php');
+require('../../../lib/ini.php');
+require('../../../lib/dbs.php');
+require('../../../lib/urldn.php');
+require('../../vendor/autoload.php');
+
+$ini_conf = load_ini_configuration();
+$db = init_pdo($ini_conf);
+if (is_null($db)) {
+  throw new Exception('Storage database not reachable');
+  exit(0);
+}
+
+$abServers = explode(',', $ini_conf['addressbook']['servers']);
+if (count($abServers) <= 0) {
+  throw new Exception('Addressbook not configured');
+  exit(0);
+}
+$ldapServers = array();
+foreach($abServers as $server) {
+  $s = sprintf('ab-%s', trim($server));
+  if (!empty($ini_conf[$s]) && !empty($ini_conf[$s]['uri'])) {
+    $ldapServers[] = array(
+      'uri' => $ini_conf[$s]['uri'],
+      'ro' => !empty($ini_conf[$s]['read-only']) ? boolval($ini_conf[$s]['read-only']) : true,
+      'dn' => !empty($ini_conf[$s]['username']) ? $ini_conf[$s]['username'] : NULL,
+      'password' => !empty($ini_conf[$s]['password']) ? $ini_conf[$s]['password'] : NULL
+    );
+  }
+}
+
+if (count($ldapServers) <= 0) {
+  throw new Exception('Addressbook not configured');
+  exit(0);
+}
+
+$ldap_db = new artnum\LDAPDB(
+   $ldapServers,
+   !empty($ini_conf['addressbook']['base']) ? $ini_conf['addressbook']['base'] : NULL
+ );
+
+$SS = \PhpOffice\PhpSpreadsheet\IOFactory::load('./basefile.xlsx');
+
+$stmt = $db->prepare('SELECT *,COALESCE((SELECT CAST(SUM("paiement_amount") AS FLOAT) FROM "paiement" WHERE "paiement_facture" = "facture_id"),0.0) AS facture_paid FROM facture WHERE  facture_paid < "facture_amount" AND facture_deleted = 0');
+$stmt->execute();
+
+$xlsRow = 10;
+$xlsSheet = $SS->getSheetByName('Créanciers');
+$now = new DateTime();
+$ldap = $ldap_db->_con();
+while(($row = $stmt->fetch(\PDO::FETCH_ASSOC))!==FALSE) {
+  if (intval($row['facture_type']) !== 1) { continue; }
+  
+  $dateBill = new DateTime($row['facture_date']);
+  $dateDue = new DateTime($row['facture_duedate']);
+  $payTime = $dateBill->diff($dateDue);
+  $overDue = $now->diff($dateDue);
+  
+
+  $rappelStmt = $db->prepare('SELECT * FROM facture WHERE facture_id IN (SELECT factureLien_source FROM factureLien WHERE factureLien_destination =:fid AND factureLien_type > 1) AND facture_deleted = 0');
+  $rappelStmt->bindParam(':fid', $row['facture_id'], \PDO::PARAM_INT);
+  $rappelStmt->execute();
+  $rappel = 0;
+  while ($rappelStmt->fetch() !== FALSE) {
+    $rappel++;
+  }
+
+  $xlsSheet->setCellValue("A$xlsRow", 'F:' . $row['facture_id']);
+  $xlsSheet->setCellValue("B$xlsRow", $dateBill);
+  $xlsSheet->setCellValue("C$xlsRow", $row['facture_reference']);
+
+  $person = '';
+  if (strpos($row['facture_person'], 'Contact/') === 0) {
+    $res = @ldap_read($ldap, url2dn($row['facture_person'], $ldap_db->getBase()), '(objectclass=*)', ['displayname', 'givenname', 'sn', 'o']);
+    if ($res) {
+      $entries = ldap_get_entries($ldap, $res);
+      if ($entries['count'] > 0) {
+        $entry = $entries[0];
+        if (!empty($entry['displayname']) && $entry['displayname']['count'] > 0) {
+           $person = trim($entry['displayname'][0]);
+        } else if (!empty($entry['o']) &&$entry['o']['count'] > 0) {
+           $person = trim($entry['o'][0]);
+        } else if ((!empty($entry['givenname']) && $entry['givenname']['count'] > 0) || (!empty($entry['sn']) && $entry['sn']['count'] > 0)) {
+           $name = !empty($entry['givenname']) && $entry['givenname']['count'] > 0 ? $entry['givenname'][0] : '';
+           $name .= $name !== '' ? ' ' : '';
+           $name .= !empty($entry['sn']) && $entry['sn']['count'] > 0 ? $entry['sn'][0] : '';
+           $person = trim($name);
+        }
+     }
+    }
+  } else {
+    $person = $row['facture_person'];
+  }
+  $xlsSheet->setCellValue("E$xlsRow", $person);
+
+  $diff = $dateBill->diff($dateDue);
+  $xlsSheet->setCellValue("F$xlsRow", $payTime->format('%a'));
+  
+  $xlsSheet->setCellValue("G$xlsRow", floatval($row['facture_amount']) - floatval($row['facture_paid']));
+  if (floatval($row['facture_paid']) > 0) {
+    $xlsSheet->setCellValue("L$xlsRow", floatval($row['facture_amount']));
+  } else {
+    $xlsSheet->setCellValue("L$xlsRow", '');
+  }
+
+  $xlsSheet->setCellValue("I$xlsRow", strtoupper($row['facture_currency']));
+  $xlsSheet->setCellValue("J$xlsRow", intval($rappel));
+
+  $xlsSheet->setCellValue("K$xlsRow", intval($overDue->format('%r%a')));
+
+  $xlsRow++;
+  
+  $repStmt = $db->prepare('SELECT * FROM repartition LEFT JOIN project ON repartition_project = project_id WHERE repartition_facture = :fid');
+  $repStmt->bindParam(':fid', $row['facture_id'], \PDO::PARAM_INT);
+  $repStmt->execute();
+  while (($repRow = $repStmt->fetch(\PDO::FETCH_ASSOC)) !== FALSE) {
+    $xlsSheet->setCellValue("A$xlsRow", 'R:' . $repRow['repartition_id']);
+    $xlsSheet->setCellValue("B$xlsRow", $dateBill);
+    $xlsSheet->setCellValue("C$xlsRow", $row['facture_reference']);
+    $xlsSheet->setCellValue("D$xlsRow", $repRow['project_reference']);
+    $xlsSheet->setCellValue("E$xlsRow", $person);
+  
+    $diff = $dateBill->diff($dateDue);
+    $xlsSheet->setCellValue("F$xlsRow", $payTime->format('%a'));
+    
+    $xlsSheet->setCellValue("G$xlsRow", floatval($row['facture_amount']));
+    $projecRep = floatval($repRow['repartition_value']);
+    $projecRep = $projecRep + $projecRep * floatval(($repRow['repartition_tva'])) / 100;
+    $xlsSheet->setCellValue("H$xlsRow", $projecRep);
+
+    $xlsSheet->setCellValue("I$xlsRow", strtoupper($row['facture_currency']));
+    $xlsSheet->setCellValue("J$xlsRow", intval($rappel));
+    $xlsSheet->setCellValue("K$xlsRow", intval($overDue->format('%r%a')));
+    $xlsRow++;
+  }
+}
+
+$SSWriter = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($SS, 'Xlsx');
+$SSWriter->setPreCalculateFormulas(false);
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="GD-' . $now->format('c') . '.xlsx"');
+header('Cache-Control: max-age=0');
+$SSWriter->save('php://output');
+?>
---
+++ admin/exec/import/bill.php
@@ -0,0 +1,189 @@
+<?PHP
+require('artnum/autoload.php');
+require('../../../lib/ini.php');
+require('../../../lib/dbs.php');
+require('../../../lib/urldn.php');
+require('../../vendor/autoload.php');
+
+$ini_conf = load_ini_configuration();
+$db = init_pdo($ini_conf);
+if (is_null($db)) {
+  throw new Exception('Storage database not reachable');
+  exit(0);
+}
+
+$abServers = explode(',', $ini_conf['addressbook']['servers']);
+if (count($abServers) <= 0) {
+  throw new Exception('Addressbook not configured');
+  exit(0);
+}
+$ldapServers = array();
+foreach($abServers as $server) {
+  $s = sprintf('ab-%s', trim($server));
+  if (!empty($ini_conf[$s]) && !empty($ini_conf[$s]['uri'])) {
+    $ldapServers[] = array(
+      'uri' => $ini_conf[$s]['uri'],
+      'ro' => !empty($ini_conf[$s]['read-only']) ? boolval($ini_conf[$s]['read-only']) : true,
+      'dn' => !empty($ini_conf[$s]['username']) ? $ini_conf[$s]['username'] : NULL,
+      'password' => !empty($ini_conf[$s]['password']) ? $ini_conf[$s]['password'] : NULL
+    );
+  }
+}
+
+if (count($ldapServers) <= 0) {
+  throw new Exception('Addressbook not configured');
+  exit(0);
+}
+
+define('ID_CELL', 0);
+define('DATE_CELL', 1);
+define('REF_CELL', 2);
+define('PROJECT_CELL', 3);
+define('TERM_CELL', 5);
+define('AMOUNT_CELL', 6);
+define('REP_CELL', 7);
+define('CURRENCY_CELL', 8);
+define('DEL_CELL', 17);
+define('PAY1_CELL', 12);
+define('PAY2_CELL', 13);
+define('PAY3_CELL', 14);
+define('PAY4_CELL', 15);
+$ldap_db = new artnum\LDAPDB(
+   $ldapServers,
+   !empty($ini_conf['addressbook']['base']) ? $ini_conf['addressbook']['base'] : NULL
+ );
+
+$Reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+$Reader->setReadDataOnly(true);
+$SS = $Reader->load('/tmp/test2.xlsx');
+
+$AS = $SS->getActiveSheet();
+$today = new DateTime();
+$todayStr = $today->format('c');
+$todayInt = $today->format('U');
+
+$out = [];
+for ($i = 10; $i < 5101; $i++) { // max size
+    $row = $AS->rangeToArray("A$i:R$i", null, false, false, false)[0];
+
+    /* we stop at first empty id */
+    if (empty($row[ID_CELL])) { break; }
+
+    $id = explode(':', $row[ID_CELL]);
+    if (trim($id[0]) === 'F') {
+        // facture
+        $stmt = $db->prepare('SELECT *,COALESCE((SELECT CAST(SUM("paiement_amount") AS FLOAT) FROM "paiement" WHERE "paiement_facture" = "facture_id"),0.0) AS facture_paid FROM facture WHERE facture_id = :id');
+        $stmt->bindParam(':id', $id[1], PDO::PARAM_INT);
+        $stmt->execute();
+        $facture = $stmt->fetch();
+        
+        $up = [];
+        $st_up = [];
+        if (empty(trim($row[DEL_CELL]))) {
+            if (intval($facture['facture_deleted']) !== 0) { 
+                $up[] = ['facture_deleted', 0, PDO::PARAM_INT];
+                $st_up[] = 'facture_deleted = :facture_deleted';
+            }
+            if (strval($facture['facture_reference']) !== strval($row[REF_CELL])) {
+                $up[] = ['facture_reference', $row[REF_CELL], PDO::PARAM_STR];
+                $st_up[] = 'facture_reference = :facture_reference';
+            }
+            if ($facture['facture_currency'] !== strtolower($row[CURRENCY_CELL])) {
+                $up[] = ['facture_currency', $row[CURRENCY_CELL], PDO::PARAM_STR];
+                $st_up[] = 'facture_currency = :facture_currency';
+            }
+            if (floatval($facture['facture_amount']) !== floatval($row[AMOUNT_CELL])) {
+                $up[] = ['facture_amount', strval(floatval($row[AMOUNT_CELL])), PDO::PARAM_STR];
+                $st_up[] = 'facture_amount = :facture_amount';
+            }
+            $out[$i] = ['type' => 'facture', 'id' => $id[1], 'op' => 'update', 'success' => false];
+        } else {
+            if (intval($facture['facture_deleted']) === 0) { 
+                $up[] = ['facture_deleted', $todayInt, PDO::PARAM_INT];
+                $st_up[] = 'facture_deleted = :facture_deleted';
+                $out[$i] = ['type' => 'facture', 'id' => $id[1], 'op' => 'delete', 'success' => false];
+            }
+        }
+        if (!empty($up)) {
+            try {
+                $stmt = $db->prepare('UPDATE facture SET ' . implode(', ', $st_up) . ' WHERE facture_id = :id');
+                foreach ($up as $v) {
+                    $stmt->bindParam(':' . $v[0], $v[1], $v[2]);
+                }
+                $stmt->bindParam(':id', $id[1], PDO::PARAM_INT);
+                $stmt->execute();
+            } catch(Exception $e) {
+                $out[$i]['success'] = false;
+            }
+        } else {
+            unset($out[$i]); // nothing done 
+        }
+
+        $total_paid = 0;
+        foreach ([PAY1_CELL, PAY2_CELL, PAY3_CELL, PAY4_CELL] as $pcell) {
+            $val = trim($row[$pcell]);
+            if (!empty($val)) {
+                if (is_numeric($val)) {
+                    $total_paid += floatval($val);
+                } else {
+                    $total_paid = floatval($facture['facture_amount']) - floatval($facture['facture_paid']);
+                }
+            }
+        }
+        if ($total_paid > floatval($facture['facture_amount']) - floatval($facture['facture_paid'])) {
+            $total_paid = floatval($facture['facture_amount']) - floatval($facture['facture_paid']);
+        }
+        if ($total_paid > 0) {
+            try {
+                $stmt = $db->prepare('INSERT INTO paiement (paiement_facture, paiement_date, paiement_amount) VALUES (:id, :date, :amount);');
+                $stmt->bindParam(':id', $id[1], PDO::PARAM_INT);
+                $stmt->bindParam(':date', $todayStr, PDO::PARAM_STR);
+                $stmt->bindParam(':amount', strval($total_paid), PDO::PARAM_STR);
+                $stmt->execute();
+                $out[$i + 5101] = ['type' => 'facture', 'id' => $id[1], 'op' => 'update', 'success' => true];
+            } catch (Exception $e) {
+                $out[$i + 5101] = ['type' => 'facture', 'id' => $id[1], 'op' => 'update', 'success' => false];;
+            }
+        }
+    } else {
+        try {
+            $stmt = $db->prepare('SELECT * FROM repartition WHERE repartition_id = :id');
+            $stmt->bindParam(':id', $id[1], PDO::PARAM_INT);
+            $stmt->execute();
+            $repartition = $stmt->fetch();
+            if ($repartition) {
+                if (!empty(trim($row[DEL_CELL]))) {
+                    try {
+                        $stmt = $db->prepare('DELETE FROM repartition WHERE repartition_id = :id');
+                        $stmt->bindParam(':id', $id[1], PDO::PARAM_STR);
+                        $stmt->execute();
+                        $out[$i] = ['type' => 'repartition', 'id' => $id[1], 'op' => 'delete', 'success' => true];
+                    } catch (Exception $e) {
+                        $out[$i] = ['type' => 'repartition', 'id' => $id[1], 'op' => 'delete', 'success' => false];
+                    }
+                } else {
+                    $amount = floatval($repartition['repartition_value']) + (floatval($repartition['repartition_value']) * floatval($repartition['repartition_tva']) / 100);
+                    if (abs(floatval($row[REP_CELL])- floatval($amount)) >= 0.0000001) {
+                        echo abs(floatval($row[REP_CELL])- floatval($amount)) . '<br>' . "\n";
+                        try {
+                            /* stored in db without tva so calculate amount without tva  */
+                            $amount = floatval($row[REP_CELL])  / (1 + $repartition['repartition_tva'] / 100);
+                            $stmt = $db->prepare('UPDATE repartition SET repartition_value = :amount WHERE repartition_id = :id');
+                            $stmt->bindParam(':amount', strval($amount), PDO::PARAM_STR);
+                            $stmt->bindParam(':id', $id[1], PDO::PARAM_INT);
+                            $stmt->execute();
+                            $out[$i] = ['type' => 'repartition', 'id' => $id[1], 'op' => 'update', 'success' => true];
+                        } catch (Exception $e) {
+                            $out[$i] = ['type' => 'repartition', 'id' => $id[1], 'op' => 'update', 'success' => false];
+                        }
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            $out[$i] = ['type' => 'repartition', 'id' => $id[1], 'op' => 'select', 'success' => false];
+        }
+    }
+}
+
+echo json_encode($out, JSON_PRETTY_PRINT);
+?>
