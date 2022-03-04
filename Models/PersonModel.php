5031f24be72ab3e8c8d9c4758c66f30b376efb60
artnum <etienne@artisan-numerique.ch>
2022-03-04 15:52:37 +0100

    Fix date to mysql date

--- Models/PersonModel.php
+++ Models/PersonModel.php
@@ -10,7 +10,7 @@ class PersonModel extends artnum\SQL {
     $this->conf('mtime.ts', true);
     $this->conf('delete', 'person_deleted');
     $this->conf('delete.ts', true);
-    $this->set_req('get', 'SELECT "person".*, COALESCE("prixheure_value", 0.0) AS "person_price" FROM "\\Table" LEFT JOIN "prixheure" ON "prixheure_id" = (SELECT "prixheure_id" FROM "prixheure" WHERE "prixheure_person" = "person_id" AND DATE("prixheure_validity") <= DATE() ORDER BY "prixheure_validity" DESC, "prixheure_value" DESC LIMIT 1)');
+    $this->set_req('get', 'SELECT "person".*, COALESCE("prixheure_value", 0.0) AS "person_price" FROM "\\Table" LEFT JOIN "prixheure" ON "prixheure_id" = (SELECT "prixheure_id" FROM "prixheure" WHERE "prixheure_person" = "person_id" AND DATE("prixheure_validity") <= DATE(NOW()) ORDER BY "prixheure_validity" DESC, "prixheure_value" DESC LIMIT 1)');
   }
 }
 ?>
