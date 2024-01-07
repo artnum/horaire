6a8a5d9b04dbc56bba3ea5a79ace62a51b025e64
Etienne Bagnoud <etienne@artisan-numerique.ch>
2024-01-07 15:05:17 +0100

    If no bexio don't setup class

--- store.php
+++ store.php
@@ -22,10 +22,12 @@ if (is_null($pdo)) {
   throw new Exception('Storage database not reachable');
   exit(0);
 }
-$bexioDB = new BizCuit\BexioCTX($KConf->get('bexio.token'));
-$store->add_db('sql', $pdo);
-$store->add_db('bexio', $bexioDB);
-$KConf->setVar('bexioDB', $bexioDB);
+if (intval($KConf->get('bexio.enabled')) !== 0) {
+  $bexioDB = new BizCuit\BexioCTX($KConf->get('bexio.token'));
+  $store->add_db('sql', $pdo);
+  $store->add_db('bexio', $bexioDB);
+  $KConf->setVar('bexioDB', $bexioDB);
+}
 
 $memcache = new Memcached();
 $memcache->addServer('localhost', 11211);
