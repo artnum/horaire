80f16a96d3744ac6ce2b7a703026177b2346f002
Etienne Bagnoud <etienne@artisan-numerique.ch>
2024-05-03 13:47:13 +0200

    Base26 (A->Z numbering)

---
+++ lib/KAAL/Utils/Base26.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KAAL\Utils;
+
+/**
+ * A to Z base26 encoding/decoding. 
+ * @package KAAL\Utils
+ */
+class Base26 {
+    static public function encode (int $num):string {
+        $res = '';
+        for ($i = 0; $i >= 0;) {
+            $res = chr(65 + ($i % 26)) . $res;
+            $i = floor($i / 26) - 1;
+        }
+        return strtoupper($res);
+    }
+    static public function decode(string $str): int {
+        $str = strtoupper($str);
+        $res = 0;
+        $len = strlen($str);
+        for ($i = 0; $i < $len; $i++) {
+            $res = ($res * 26) + (ord($str[$i]) - 64);
+        }
+        return $res - 1;
+    }
+}
