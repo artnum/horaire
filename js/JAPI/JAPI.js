06e3bed1690a3217243bb5a47b9b7d7872136ca2
Etienne Bagnoud <etienne@artisan-numerique.ch>
2025-01-31 10:47:36 +0100

    Design, add auth japi

--- js/JAPI/JAPI.js
+++ js/JAPI/JAPI.js
@@ -3,6 +3,12 @@ import { PJApi } from './$script/vendor/pjAPI/src/pjapi.mjs'
 export class JAPI {
     constructor() {
         this.API = PJApi.instance
-        this.API.open(new URL('$api', window.location).toString())
+        this.API.open(
+            new URL('$api', window.location).toString(),
+            80,
+            {
+                'Authorization': `Bearer ${localStorage.getItem('klogin-token')}`
+            }
+        )
     }
 }
