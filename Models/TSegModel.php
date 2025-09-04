9f718d0f9d48843ffcd456407755fc3307bb906a
Etienne Bagnoud <etienne@artisan-numerique.ch>
2025-09-04 11:37:22 +0200

    Remove old file

--- Models/TSegModel.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-class TSegModel extends artnum\SQL {
-  function __construct($db, $config) {
-    $this->kconf = $config;
-    parent::__construct($db, 'tseg', 'tseg_id', []);
-    $this->conf('auto-increment', true);
-  }
-}
-?>
