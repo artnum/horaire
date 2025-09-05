9351a9fef46124545c571b92dd6c5777c1f7ba9a
Etienne Bagnoud <etienne@artisan-numerique.ch>
2025-09-05 15:54:46 +0200

    Wrong base64 decoding

--- lib/KAAL/Auth.php
+++ lib/KAAL/Auth.php
@@ -270,7 +270,7 @@ class Auth
     {
         $crypto = new Crypto($hash);
         $sign = $crypto->get_random_bytes();
-        $authvalue = $crypto->stringify($crypto->hmac($sign . $cnonce, base64_decode($hpw)));
+        $authvalue = $crypto->stringify($crypto->hmac($sign . $cnonce, Base64::decode($hpw)));
         if ($this->add_auth($userid, $authvalue, '', self::SHARE_NONE)) {
             return $crypto->stringify($sign);
         }
@@ -286,7 +286,7 @@ class Auth
         }
         $crypto = new Crypto($hash);
         $sign = $crypto->get_random_bytes();
-        $share_authvalue = $crypto->stringify($crypto->hmac($sign, base64_decode($authvalue)));
+        $share_authvalue = $crypto->stringify($crypto->hmac($sign, Base64::decode($authvalue)));
         if ($this->add_auth($userid, $share_authvalue, $this->prepare_url($url), $permanent, $comment, $duration)) {
             return $share_authvalue;
         }
