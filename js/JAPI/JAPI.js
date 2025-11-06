51563449ad3f2314834c23e7d91e58d1a4db093b
Etienne Bagnoud <etienne@artisan-numerique.ch>
2025-11-06 14:43:42 +0100

    Extend but less

--- js/JAPI/JAPI.js
+++ js/JAPI/JAPI.js
@@ -3,7 +3,7 @@ import PJApi from './$script/vendor/pjAPI/src/pjapi.mjs'
 export class JAPI {
   constructor() {
     this.API = PJApi.instance
-    this.API.open(new URL('$api', window.location).toString(), 160, {
+    this.API.open(new URL('$api', window.location).toString(), 100, {
       Authorization: `Bearer ${localStorage.getItem('klogin-token')}`,
     })
   }
