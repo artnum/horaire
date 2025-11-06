ea831db2f720d4b80ac8e011e93a5e77b50397c8
Etienne Bagnoud <etienne@artisan-numerique.ch>
2025-11-06 14:38:04 +0100

    Extend timeout

--- js/JAPI/JAPI.js
+++ js/JAPI/JAPI.js
@@ -3,13 +3,9 @@ import PJApi from './$script/vendor/pjAPI/src/pjapi.mjs'
 export class JAPI {
   constructor() {
     this.API = PJApi.instance
-    this.API.open(
-      new URL('$api', window.location).toString(),
-      80,
-      {
-        'Authorization': `Bearer ${localStorage.getItem('klogin-token')}`
-      }
-    )
+    this.API.open(new URL('$api', window.location).toString(), 160, {
+      Authorization: `Bearer ${localStorage.getItem('klogin-token')}`,
+    })
   }
 }
 
@@ -24,44 +20,40 @@ export class JAPIIterator {
 
   next() {
     return new Promise((resolve, reject) => {
-      if (this.offset === this.end) { return resolve([]) }
+      if (this.offset === this.end) {
+        return resolve([])
+      }
       if (this.offset + this.size <= this.loaded.length) {
         this.offset += this.size
-        return resolve(
-          this.loaded.slice(this.offset - this.size, this.offset)
-        )
+        return resolve(this.loaded.slice(this.offset - this.size, this.offset))
       }
       this.callback(this.offset, this.size)
         .then(([objects, last]) => {
           if (last) {
             this.end = this.offset + objects.length
           }
-          this.loaded = [
-            ...this.loaded,
-            ...objects
-          ]
+          this.loaded = [...this.loaded, ...objects]
           this.offset += objects.length
           return resolve(
-            this.loaded.slice(
-              this.offset - objects.length,
-              this.offset
-            )
+            this.loaded.slice(this.offset - objects.length, this.offset),
           )
         })
-        .catch(e => reject(e))
+        .catch((e) => reject(e))
     })
   }
 
   previous() {
     return new Promise((resolve, _) => {
-      if (this.offset === 0) { return resolve([]) }
+      if (this.offset === 0) {
+        return resolve([])
+      }
       const previous = this.offset
       this.offset -= this.size
-      if (this.offset < 0) { this.offset = 0 }
+      if (this.offset < 0) {
+        this.offset = 0
+      }
       let _size = previous - this.offset
-      return resolve(
-        this.loaded.slice(this.offset, this.offset + _size)
-      )
+      return resolve(this.loaded.slice(this.offset, this.offset + _size))
     })
   }
 
